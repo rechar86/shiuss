60f6aad0fc43e8b05247025d1242d803cc8ab0c1
rechar86 <40575870+rechar86@users.noreply.github.com>
2018-08-24 00:36:02 +0800

    V1.2.1
    add line2me

--- content.js
+++ content.js
@@ -1,4 +1,53 @@
 
+
+/**
+ * 發LINE訊息給我
+ */
+function doSHIUSS_LINE2ME(){
+	// submit onclick disable btn
+    $("#btnSubmit").prop("disabled", true);
+            
+    var formData = {
+        'order_type' : 'SHIUSS_LINE2ME',
+        'name': $('input[name=name]').val(),
+        'email':$('input[name=email]').val(),
+        'mobile':$('input[name=mobile]').val(),
+        'msg':$('textarea[name=msg]').val()
+    };
+
+    // process the form
+    $.ajax({
+        type 		: 'GET', // define the type of HTTP verb we want to use (POST for our form)
+        url 		: 'https://script.google.com/macros/s/AKfycbz9HhXguoQbEVY98kSJnccDsUsRjilPX4pmlcQY0bboptjvDfQK/exec', // the url where we want to POST
+        data 		: formData, // our data object
+        dataType 	: 'text', // what type of data do we expect back from the server
+        success: function(text){
+            // no response
+            $("#btnSubmit").prop("disabled", false);//enable button
+        },
+        error: function(e){
+            alert('失敗:' + e);
+            $("#btnSubmit").prop("disabled", false);//enable button
+        }
+    })
+        // using the done promise callback
+        .done(function(data) {
+            alert("送出完成");
+            $("#btnSubmit").prop("disabled", false);//enable button
+        })
+
+        // using the fail promise callback
+        .fail(function(data) {
+            // show any errors
+            // best to remove for production
+            alert("失敗("+data + ")");
+            $("#btnSubmit").prop("disabled", false);//enable button
+        });
+
+    // stop the form from submitting the normal way and refreshing the page
+    event.preventDefault();
+}
+
 /**
  * 小巫
  */
